eb984503f895c40cab6d058d947ea095a26147f0
rekrutivmaksim-spec <rekrutivmaksim@gmail.com>
2026-02-03 14:03:05 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,8 @@
-import {defineConfig} from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import {componentTagger} from "pp-tagger";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig(({mode}) => ({
-    plugins: [
-        react(),
-        mode === 'development' &&
-        componentTagger(),
-    ].filter(Boolean),
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
-    },
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        allowedHosts: true,
-        hmr: {
-            overlay: false // Disables the error overlay if you only want console errors
-        }
-    },
-}));
+export default defineConfig({
+  // КРИТИЧЕСКИ ВАЖНО для CI (GitHub Actions) + Capacitor
+  base: "./",
+  plugins: [react()],
+});
